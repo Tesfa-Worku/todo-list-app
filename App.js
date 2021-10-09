dfba54b673365081af900eb3ca5df2d8d9db6b37
Tesfa-Worku <hopegorg@gmail.com>
2021-10-09 00:56:41 -0400

    updates

--- App.js
+++ App.js
@@ -1,42 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import {AppMod} from './model';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, Pressable, CheckBox} from 'react-native';
 
 export default function App() {
-
   const [items, setItems] = useState(
     [
       {todo: 'first item'},
       {todo: 'second item'}
     ]
-  )
+  );
 
-    const generateList = items.map((item, index) => (
+  const [text, onChangeText] = useState("");  
+  
+  const addItemToList = () => {
+    let cleanedText = text.trim().toLowerCase();
+    if (!/^[a-zA-Z0-9- ]*$/.test(cleanedText)) {
+      return { error: "Only letters and numbers are allowed " };
+    }
+    if (cleanedText.length === 0) {
+      return { error: "That input is blank! " };
+    }
+
+    const isNotDuplicate = items.every(
+      (item) => item.todo.toLowerCase() !== cleanedText
+    );
+
+    if (isNotDuplicate) {
+      setItems([...items, { todo: text }]);
+      onChangeText("");
+    }
+
+    return isNotDuplicate
+      ? { success: "Item added to your todo list" }
+      : { error: "That todo list item is a duplicate" };
+  };
+
+  const generateList = items.map((item, index) => (
     <View key={index} style={styles.listItemContainer}>
+      <CheckBox          
+          style={styles.checkbox}
+        />
       <Text>{item.todo}</Text>
     </View>
   )
 );
-
-const [text, onChangeText] = useState("");
-
-const addItemToList = () => {
-  // console.log({todo: newItemText});
-  // console.log([...items, {todo: newItemText}]);
-  if(text.length>0 && isNaN(text)){
-    setItems([...items, {todo: text}]);
-    onChangeText("");
-  }  
-}
-
-
-
-const [isSelected, setSelection] = useState(false);
-
-const removeItemfromList = () => {
-  setSelection()
-}
 
   return (
     <View style={styles.container}>
@@ -45,39 +52,24 @@ const removeItemfromList = () => {
 
       <TextInput
         style={styles.addItemInput}
-        onChangeText={text => onChangeText(text)}
-        onSubmitEditing={addItemToList}
+        onChangeText={onChangeText}
+        onChangeText={text => onChangeText(text)}       
         value={text}
         autoFocus={true}
       />
-
-      <View style={styles.checkboxContainer}>
-        <CheckBox
-          value={isSelected}
-          onValueChange={setSelection}
-          style={styles.checkbox}
-        />
-        <Text style={styles.label}>Are you sure?</Text>
-      </View>
-
+      
       <View style={styles.butnWrap}>
         <Button
           title="Add Item"
-          color="blue"
+          color="blue"          
           onPress={addItemToList}
-        />
-
-        <Button
-          title="Remove"
-          color="blue"
-          onPress={setSelection}
         />
     
       </View>      
     
       <ScrollView style={styles.scrollView}>
           {generateList}
-      </ScrollView>
+      </ScrollView>      
 
       <StatusBar style="auto"/>
 
@@ -92,20 +84,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  appTitle:{
-    //flex: 1,
+  
+  appTitle:{    
     fontSize: 40,
-    alignItems: 'center',
-    //width: '%',
-    color: 'yellow',
-    padding: '25px',
+    alignItems: "center",    
+    color: "yellow",
+    padding: "25px",
   },
 
   listItemContainer: {
-    alignItems: 'center',
-    // width: '100%',
-    // padding: '10px', 
+    alignItems: "center",    
     width:"75",
     border:"2px solid green",
     borderRadius:"15px",
@@ -113,12 +101,11 @@ const styles = StyleSheet.create({
     margin:"1rem",
     fontSize:"1.5rem",
     textAlign:"center",
-    backgroundColor:`#ffa07a`,
-    maxWidth:350,  
+    backgroundColor:`#FFF`,
+    maxWidth:350,    
   },
 
-  addItemInput: {
-    //flex: 1,
+  addItemInput:{  
     alignItems: 'flex-start',
     backgroundColor: 'orange',
     width: '60%',
@@ -133,55 +120,12 @@ const styles = StyleSheet.create({
   },
 
   scrollView: {
-    width: '100%',
-    //alignItems: 'center',  
-  },  
-
-  checkboxContainer: {
-    flexDirection: "row",
-    marginBottom: 20,
-    justifyContent: 'space-evenly',
+    width: '100%'    
   },
 
   checkbox: {
     margin: 8,
   },
-
-  label: {
-    margin: 8,
-  },
-
-});
-
-
-
-
-
-// function MyCheckbox(props) {
-//   const number = props.number;
-//   function onCheckmarkPress() {
-//     setItems(items.filter((value, i) => i !== number));
-//   }
-//   return (
-//     <Pressable
-//       style={[styles.checkboxBase]}
-//       onPress={onCheckmarkPress}
-//     ></Pressable>
-//   );
-// }
-
-
-// checkboxBase: {
-  //   width: 24,
-  //   height: 24,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   borderRadius: 4,
-  //   borderWidth: 2,
-  //   borderColor: "coral",
-  //   backgroundColor: "red",
-  // },
-
-  <View>
-    {hello}
-  </View>
+    
+}
+);
